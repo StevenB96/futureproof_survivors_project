3eaca9bc0dae98016657c0ee231737833d8bd1dc
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2020-08-24 13:15:51 +0100

    Created a root page. Installed bootstrap locally

---
+++ auxins_job_site/resources/views/root.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <link href='#' rel='stylesheet' type = '#'><!--Fonts-->
+    <link href='../bootstrap-3.4.1-dist/css/bootstrap.min.css' rel='stylesheet' type='text/css'><!--CSS frameworks-->
+    <link href = '#' rel = 'stylesheet' type = 'text/css'/><!--Custom CSS-->
+    <script
+    src='#'
+    integrity='#'
+    crossorigin='#'
+    type='application/javascript'>
+    </script><!--JavasScript frameworks-->
+    <script  src='#'
+    type='application/javascript'>
+    </script><!--Custom JavaScript-->
+</head>
+<body>
+    <h1 class="alert alert-primary">Hello World!</h1>
+</body><!--Everything written on the page-->
+</html>
