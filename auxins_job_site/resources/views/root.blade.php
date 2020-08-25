f6421f5507f8c87bef0d5567294e9695565b4a82
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2020-08-25 19:00:35 +0100

    Generate contact details table programatically

--- auxins_job_site/resources/views/root.blade.php
+++ auxins_job_site/resources/views/root.blade.php
@@ -30,73 +30,81 @@
     </div>
     <div class="rootFooterBusinessEnquiries tableDiv">
         <p style="text-align:center; font-size: 1vw;">Contact information:</p>
+        <?php
+            $contactDetails = array(
+                "Email address" => "fakeemail@fakemail.com",
+                "Telephone" => "+44 1632 960964",
+                "Mail address" => "61 Fox Road,<br>Framingham Pigot,<br>NORWICH,<br>NR14 7PZ"
+            );
+        ?>
         <table>
-            <tr><td style="padding-right:10px;">Email address</td><td>fakeemail@fakemail.com</td></tr>
-            <tr><td style="padding-right:10px;">Telephone</td><td>+44 1632 960964</td></tr>
-            <tr><td style="padding-right:10px;">Mail address</td><td>61  Fox Road<br>Framingham Pigot<br>NORWICH<br>NR14 7PZ</td></tr>
+            <?php foreach ($contactDetails as $detail => $value):
+                echo '<tr>';
+                echo '<td style="text-align: right">' . $detail . '</td>';
+                echo '<td>' . $value . '</td>';
+                echo '</tr>';
+            endforeach; ?>
         </table>
     </div>
     <div class="rootFooterOpeningHoursDiv tableDiv">
-        <?php
-        use App\Libraries\StoreHours;
-        // REQUIRED
-        // Set your default time zone (listed here: http://php.net/manual/en/timezones.php)
-        date_default_timezone_set('Europe/London');
-        // Include the store hours class
-        // require_once(app_path() . '\Libraries\StoreHours.php');
+        <?php        
+            // REQUIRED
+            // Set your default time zone (listed here: http://php.net/manual/en/timezones.php)
+            date_default_timezone_set('Europe/London');
+            // Include the store hours class
+            use App\Libraries\StoreHours;
 
-        // REQUIRED
-        // Define daily open hours
-        // Must be in 24-hour format, separated by dash
-        // If closed for the day, leave blank (ex. sunday)
-        // If open multiple times in one day, enter time ranges separated by a comma
-        $hours = array(
-            'mon' => array('09:00-17:00'),
-            'tue' => array('09:00-17:00'),
-            'wed' => array('09:00-17:00'),
-            'thu' => array('09:00-17:00'), // Open late
-            'fri' => array('09:00-17:00'),
-            'sat' => array('09:00-17:00'),
-            'sun' => array('09:00-17:00')
-        );
+            // REQUIRED
+            // Define daily open hours
+            // Must be in 24-hour format, separated by dash
+            // If closed for the day, leave blank (ex. sunday)
+            // If open multiple times in one day, enter time ranges separated by a comma
+            $hours = array(
+                'mon' => array('09:00-17:00'),
+                'tue' => array('09:00-17:00'),
+                'wed' => array('09:00-17:00'),
+                'thu' => array('09:00-17:00'), // Open late
+                'fri' => array('09:00-17:00'),
+                'sat' => array('09:00-17:00'),
+                'sun' => array('09:00-17:00')
+            );
 
-        // OPTIONAL
-        // Add exceptions (great for holidays etc.)
-        // MUST be in a format month/day[/year] or [year-]month-day
-        // Do not include the year if the exception repeats annually
-        $exceptions = array(
-            '2/24'  => array('11:00-18:00'),
-            '10/18' => array('11:00-16:00', '18:00-20:30')
-        );
+            // OPTIONAL
+            // Add exceptions (great for holidays etc.)
+            // MUST be in a format month/day[/year] or [year-]month-day
+            // Do not include the year if the exception repeats annually
+            $exceptions = array(
+                '2/24'  => array('11:00-18:00'),
+                '10/18' => array('11:00-16:00', '18:00-20:30')
+            );
 
-        $config = array(
-            'separator'      => ' - ',
-            'join'           => ' and ',
-            'format'         => 'g:ia',
-            'overview_weekdays'  => array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-        );
+            $config = array(
+                'separator'      => ' - ',
+                'join'           => ' and ',
+                'format'         => 'g:ia',
+                'overview_weekdays'  => array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+            );
 
-        // Instantiate class
-        $store_hours = new StoreHours($hours, $exceptions, $config);
-        
-        // Display open / closed message
-        if($store_hours->is_open()) {
-            echo '<p style="color: green;">We are open!</p><p>Normal office hours:</p>';
-        } else {
-            echo '<p style="color: red;">We are closed.</p><p>Normal office hours:</p>';
-        }
+            // Instantiate class
+            $store_hours = new StoreHours($hours, $exceptions, $config);
+            
+            // Display open / closed message
+            if($store_hours->is_open()) {
+                echo '<p style="color: green;">We are open!</p><p>Normal office hours:</p>';
+            } else {
+                echo '<p style="color: red;">We are closed.</p><p>Normal office hours:</p>';
+            }
 
-        // Display full list of open hours (for a week without exceptions)
-        echo '<table>';
-        foreach ($store_hours->hours_this_week() as $days => $hours) {
-            echo '<tr>';
-            echo '<td>' . $days . '</td>';
-            echo '<td>' . $hours . '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
+            // Display full list of open hours (for a week without exceptions)
+            echo '<table>';
+            foreach ($store_hours->hours_this_week() as $days => $hours) {
+                echo '<tr>';
+                echo '<td style="text-align: right">' . $days . '</td>';
+                echo '<td>' . $hours . '</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
         ?>
-
     </div>
 </body>
 
