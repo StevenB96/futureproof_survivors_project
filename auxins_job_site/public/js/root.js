b990f457dd614e524f4175f7d23f715c6113eee2
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2020-08-27 20:38:56 +0100

    Changed asset links, began to intergrate bootstrap

--- auxins_job_site/public/js/root.js
+++ auxins_job_site/public/js/root.js
@@ -1,26 +1,9 @@
 $(document).ready(function() {
   $("#rootFooter").css({
-    'height': ($("#rootHeader").height() + 'px'),
+    'height': ($("#rootHeader").height() + 'px')
   });
-  $(".rootBody").css({
-    'grid-template-rows': ('auto auto' + $(".rootHeader").height() + 'px')
-  });
-  $("table *, .rootP").css({
-    'font-size': ($("#rootHeader").width()/55.8 + 'px')
-  });
-  $("table").css({
-    'border-spacing': ($(".rootHeader").width()/446.4 + 'px')
-  });
-  $("table td, table th").css({
-    'padding': ('0px ' + $(".rootHeader").width()/178.56 + 'px'),
-  });
-});
-$(window).on('resize', function(){
-  $("#rootFooter").css({
-    'height': ($("#rootHeader").height() + 'px'),
-  });
-  $(".rootBody").css({
-    'grid-template-rows': ('auto auto' + $(".rootHeader").height() + 'px')
+  $("#rootBody").css({
+    'grid-template-rows': ('auto auto' + $("#rootHeader").height() + 'px')
   });
   $("table *, .rootP").css({
     'font-size': ($("#rootHeader").width()/55.8 + 'px')
@@ -29,6 +12,23 @@ $(window).on('resize', function(){
     'border-spacing': ($("#rootHeader").width()/446.4 + 'px')
   });
   $("table td, table th").css({
-    'padding': ('0px ' + $("#rootHeader").width()/178.56 + 'px'),
+    'padding': ('0px ' + $("#rootHeader").width()/178.56 + 'px')
+  });
+});
+$(window).on('resize', function(){
+  $("#rootFooter").css({
+    'height': ($("#rootHeader").height() + 'px')
+  });
+  $("#rootBody").css({
+    'grid-template-rows': ('auto auto' + $("#rootHeader").height() + 'px')
+  });
+  $("table *, .rootP").css({
+    'font-size': ($("#rootHeader").width()/55.8 + 'px')
+  });
+  $("table").css({
+    'border-spacing': ($("#rootHeader").width()/446.4 + 'px')
+  });
+  $("table td, table th").css({
+    'padding': ('0px ' + $("#rootHeader").width()/178.56 + 'px')
   });
 });
--- auxins_job_site/resources/views/root.blade.php
+++ auxins_job_site/resources/views/root.blade.php
@@ -5,13 +5,17 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Document</title>
     <link href='#' rel='stylesheet' type = '#'><!--Fonts-->
-    <link href="bootstrap-3.4.1-dist\css\bootstrap.min.css" rel='stylesheet' type='text/css'><!--CSS frameworks-->
+    <link href="{{ asset('bootstrap-3.4.1-dist\css\bootstrap.min.css') }}" rel='stylesheet' type='text/css'><!--CSS frameworks-->
     <link href = "{{ asset('css\root.css') }}" rel = 'stylesheet' type = 'text/css'/><!--Custom CSS-->
     <script
-    src="jquery-3.5.1.min.js"
+    src="{{ asset('jquery-3.5.1.min.js') }}"
+    type='application/javascript'>
+    </script>
+    <script
+    src="{{ asset('bootstrap-3.4.1-dist\js\bootstrap.min.js') }}"
     type='application/javascript'>
     </script><!--JavasScript frameworks-->
-    <script src="js/root.js"
+    <script src="js\root.js"
     type='application/javascript'>
     </script><!--Custom JavaScript-->
 </head>
