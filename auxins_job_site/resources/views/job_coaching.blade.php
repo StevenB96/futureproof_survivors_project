afa3d5c59345e757206a4352b7e46f20776a4c1f
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2020-08-27 21:57:37 +0100

    Update job_coaching.blade.php

--- auxins_job_site/resources/views/job_coaching.blade.php
+++ auxins_job_site/resources/views/job_coaching.blade.php
@@ -1,18 +1,19 @@
 @extends('root')
 
 @section('content')
-    <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand text-dark font-weight-bold">JOB COACHING</a>
-        <a href="http://127.0.0.1:8000/about" class="navbar-item text-dark font-weight-bold">About</a>
-        <a href="http://127.0.0.1:8000/job_coaching" class="navbar-item text-dark font-weight-bold">Digital CV Builder</a>
+    <nav class="navbar navbar-light bg-light" style="padding-top: 0px; padding-bottom: 0px;">
+        <a class="navbar-brand text-dark font-weight-bold">Job Coaching</a>
+        <a href="http://127.0.0.1:8000/about" class="navbar-item text-dark">About</a>
+        <a href="http://127.0.0.1:8000/job_coaching" class="navbar-item text-dark">Digital CV Builder</a>
         <div class="btn-group">
-        <button type="button" class="navbar-toggler" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="navbar-toggler-icon"></span>
-        </button>  
-        <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item font-weight-bold" type="button">Account</button>
-            <button class="dropdown-item font-weight-bold" type="button">Another action</button>
-            <button class="dropdown-item font-weight-bold" type="button">Another action</button>
+            <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Settings
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+                <button class="dropdown-item" type="button">Action</button>
+                <button class="dropdown-item" type="button">Another action</button>
+                <button class="dropdown-item" type="button">Something else here</button>
+            </div>
         </div>
     </nav>
 @endsection
