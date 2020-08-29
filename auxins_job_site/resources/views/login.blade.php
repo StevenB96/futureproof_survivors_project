93aeb3395d997a6b8bc80c4d7951d331bb06eb8b
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2020-08-29 16:36:56 +0100

    Added some basic checking to the register and login forms

--- auxins_job_site/resources/views/login.blade.php
+++ auxins_job_site/resources/views/login.blade.php
@@ -9,16 +9,16 @@
         <div style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 1; grid-row-end: 2; text-align: center;">
             <strong class="navStrong indexText">Login</strong><a class="nav-link" href="http://127.0.0.1:8000/register">Don't have an account? Click here to register.</a>
         </div>
-        <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 2; grid-row-end: 3;">
+        <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 4; grid-row-end: 5;">
             <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" name="email" required>
         </div>
-        <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 3; grid-row-end: 4;">
+        <div class="form-group" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: 5; grid-row-end: 6;">
             <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd">
+            <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="checkbox" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: 4; grid-row-end: 5; text-align: center;">
-            <label>Remember me <input type="checkbox"> </label>
+            <label>Remember me <input type="checkbox" name="remember_me"></label required>
         </div>
         <button type="submit" class="btn btn-secondary indexText" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: 4; grid-row-end: 5;">Submit</button>
     </form>
--- auxins_job_site/resources/views/register.blade.php
+++ auxins_job_site/resources/views/register.blade.php
@@ -10,27 +10,27 @@
             <strong class="navStrong indexText">Register</strong><a class="nav-link" href="http://127.0.0.1:8000/login">Already have an account? Click here to login.</a>
         </div>
         <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 2; grid-row-end: 3;">
-            <label for="email">Forename:</label>
-            <input type="email" class="form-control" id="email">
+            <label for="forename">Forename:</label>
+            <input type="text" class="form-control" id="forename" name="forename" required>
         </div>
         <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 3; grid-row-end: 4;">
             <label for="email">Surname:</label>
-            <input type="email" class="form-control" id="email">
+            <input type="text" class="form-control" id="surname" name="surname" required>
         </div>    
         <div class="form-group" style="grid-column-start: 1; grid-column-end: 3; grid-row-start: 4; grid-row-end: 5;">
             <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" name="email" required>
         </div>
         <div class="form-group" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: 5; grid-row-end: 6;">
             <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd">
+            <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="form-group" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: 5; grid-row-end: 6;">
             <label for="pwd">Re-type password:</label>
-            <input type="password" class="form-control" id="pwd">
+            <input type="password" class="form-control" id="password" name="password_confirm" required>
         </div>
         <div class="checkbox" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: 6; grid-row-end: 7; text-align: center;">
-            <label>Remember me <input type="checkbox"> </label>
+            <label>Remember me <input type="checkbox" name="remember_me"></label required>
         </div>
         <button type="submit" class="btn btn-secondary indexText" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: 6; grid-row-end: 7;">Submit</button>
     </form>
